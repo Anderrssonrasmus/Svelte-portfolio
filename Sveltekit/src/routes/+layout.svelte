915c84bc73aa9c6aa3d3bc9be7d953aaa4a6f9fc
Razzey <rasmus022.ra@gmail.com>
2023-08-04 00:16:25 +0200

    fixed image paths on start

--- Sveltekit/src/routes/+layout.svelte
+++ Sveltekit/src/routes/+layout.svelte
@@ -1,18 +1,17 @@
 <script>
-
-import Logo from "../../static/logo.svg"
-import "../../src/global.css";
+    import Logo from "/images/logo.svg";
+    import "../../src/global.css";
 </script>
 
 <header>
     <div class="corner-lines">
-        <a href="#"  class="logo">
-            <img src={Logo} alt="">
+        <a href="#" class="logo">
+            <img src={Logo} alt="" />
         </a>
         <div class="lines">
-            <div></div>
-            <div></div>
-            <div></div>
+            <div />
+            <div />
+            <div />
         </div>
     </div>
 </header>
@@ -21,135 +20,131 @@ import "../../src/global.css";
 
 <footer>
     <div class="corner-lines">
-        <a href="#"  class="logo">
-            <div></div>
+        <a href="#" class="logo">
+            <div />
         </a>
         <div class="lines">
-            <div></div>
-            <div></div>
-            <div></div>
+            <div />
+            <div />
+            <div />
         </div>
     </div>
 </footer>
 
 <style lang="scss">
-
-header {
-    .corner-lines {
-        height: 203px;
-        width: 203px;
-        position: fixed;
-        top: 0;
-        left: 0;
-        margin: 32px;
-
-        .logo {
-            img {
-                width: 64px;
-                transform: rotate(-45deg);
-                margin: 32px;
-            }
-        }
-
-        .lines {
-            position: absolute;
+    header {
+        .corner-lines {
+            height: 203px;
+            width: 203px;
+            position: fixed;
             top: 0;
             left: 0;
-            right: 0;
-            bottom: 0;
+            margin: 32px;
 
-            div {
-                background: white;
-                display: block;
+            .logo {
+                img {
+                    width: 64px;
+                    transform: rotate(-45deg);
+                    margin: 32px;
+                }
+            }
+
+            .lines {
                 position: absolute;
+                top: 0;
+                left: 0;
+                right: 0;
+                bottom: 0;
 
-                &:nth-child(1) {
-                    top: 0;
-                    left: 50px;
-                    width: calc(100% - 50px);
-                    height: 1px;
-                }
+                div {
+                    background: white;
+                    display: block;
+                    position: absolute;
 
-                &:nth-child(2) {
-                    left: 0;
-                    top: 50px;
-                    height: calc(100% - 50px);
-                    width: 1px;
-                }
+                    &:nth-child(1) {
+                        top: 0;
+                        left: 50px;
+                        width: calc(100% - 50px);
+                        height: 1px;
+                    }
 
-                &:nth-child(3) {
-                    left: 0;
-                    top: -20px;
-                    height: 70px;
-                    width: 1.5px;
-                    transform: rotate(45deg);
-                    transform-origin: bottom;
+                    &:nth-child(2) {
+                        left: 0;
+                        top: 50px;
+                        height: calc(100% - 50px);
+                        width: 1px;
+                    }
+
+                    &:nth-child(3) {
+                        left: 0;
+                        top: -20px;
+                        height: 70px;
+                        width: 1.5px;
+                        transform: rotate(45deg);
+                        transform-origin: bottom;
+                    }
                 }
             }
         }
-
     }
-}
 
-footer {
-    .corner-lines {
-        height: 203px;
-        width: 203px;
-        position: fixed;
-        bottom: 0;
-        right: 0;
-        margin: 32px;
-        display: flex;
-        justify-content: end;
-        align-items: end;
-
-        .logo {
-            div {
-                margin: 32px;
-                transform: rotate(-225deg);
-                border: 32px solid transparent;
-                border-top-color: white;
-            }
-        }
-
-        .lines {
-            position: absolute;
-            top: 0;
-            left: 0;
-            right: 0;
+    footer {
+        .corner-lines {
+            height: 203px;
+            width: 203px;
+            position: fixed;
             bottom: 0;
+            right: 0;
+            margin: 32px;
+            display: flex;
+            justify-content: end;
+            align-items: end;
 
-            div {
-                background: white;
-                display: block;
+            .logo {
+                div {
+                    margin: 32px;
+                    transform: rotate(-225deg);
+                    border: 32px solid transparent;
+                    border-top-color: white;
+                }
+            }
+
+            .lines {
                 position: absolute;
+                top: 0;
+                left: 0;
+                right: 0;
+                bottom: 0;
 
-                &:nth-child(1) {
-                    bottom: 0;
-                    right: 50px;
-                    width: calc(100% - 50px);
-                    height: 1px;
-                }
+                div {
+                    background: white;
+                    display: block;
+                    position: absolute;
 
-                &:nth-child(2) {
-                    right: 0;
-                    bottom: 50px;
-                    height: calc(100% - 50px);
-                    width: 1px;
-                }
+                    &:nth-child(1) {
+                        bottom: 0;
+                        right: 50px;
+                        width: calc(100% - 50px);
+                        height: 1px;
+                    }
 
-                &:nth-child(3) {
-                    right: 0;
-                    bottom: -20px;
-                    height: 70px;
-                    width: 1.5px;
-                    transform: rotate(45deg);
-                    transform-origin: top;
+                    &:nth-child(2) {
+                        right: 0;
+                        bottom: 50px;
+                        height: calc(100% - 50px);
+                        width: 1px;
+                    }
+
+                    &:nth-child(3) {
+                        right: 0;
+                        bottom: -20px;
+                        height: 70px;
+                        width: 1.5px;
+                        transform: rotate(45deg);
+                        transform-origin: top;
+                    }
                 }
             }
         }
-
     }
-}
-
 </style>
--- Sveltekit/src/routes/+page.svelte
+++ Sveltekit/src/routes/+page.svelte
@@ -1,10 +1,5 @@
-<script>
-
-    import barcode from "../../static/barcode.png"
-
-    document.body.style.overflow = "hidden";
-    document.body.style.backgroundImage = "url(../../static/barcode.png)";
-    
+<script lang="js">
+    import Barcode from "/images/barcode.png";
 </script>
 
 <div class="titleCard">
@@ -12,58 +7,54 @@
     <h2>Portfolio_</h2>
 </div>
 <div class="barcode">
-    <img src={barcode} alt="">
+    <img srcset={Barcode} alt="" />
     <a href="#">Continue</a>
 </div>
 
-
-
 <style lang="scss">
-
-.titleCard {
-    font-family: "Kleenmax";
-    color: white;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    text-transform: uppercase;
-    text-align: center;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -75%);
-    gap: 32px;
-
-    h1 {
-        font-size: 128px;
-    }
-
-    h2 {
-        font-size: 64px;
-        color: transparent;
-        -webkit-text-stroke: 1px white;
-    }
-}
-
-.barcode {
-    position: absolute;
-    bottom: -56px;
-    left: 50%;
-    transform: translateX(-50%);
-
-    a {
+    .titleCard {
+        font-family: "Kleenmax";
         color: white;
-        mix-blend-mode: difference;
-        text-decoration: none;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        text-transform: uppercase;
+        text-align: center;
         position: absolute;
         top: 50%;
         left: 50%;
         transform: translate(-50%, -75%);
-        font-size: 64px;
-        text-transform: uppercase;
-        font-family: "Krona";
-    }
-}
+        gap: 32px;
 
+        h1 {
+            font-size: 128px;
+        }
+
+        h2 {
+            font-size: 64px;
+            color: transparent;
+            -webkit-text-stroke: 1px white;
+        }
+    }
+
+    .barcode {
+        position: absolute;
+        bottom: -56px;
+        left: 50%;
+        transform: translateX(-50%);
+
+        a {
+            color: white;
+            mix-blend-mode: difference;
+            text-decoration: none;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -75%);
+            font-size: 64px;
+            text-transform: uppercase;
+            font-family: "Krona";
+        }
+    }
 </style>
