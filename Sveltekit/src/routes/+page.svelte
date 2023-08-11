9a984835d39349e5a9518b70f08b4b0a2a1499dc
RasmusFostira <rasmus.andersson@fostira.se>
2023-08-11 16:58:54 +0200

    Projects section

--- Sveltekit/src/routes/+page.svelte
+++ Sveltekit/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="js">
     import Barcode from "/images/barcode.png";
+    import Logo from "/images/logo.svg";
 </script>
 
 <section class="hero">
@@ -14,7 +15,42 @@
 </section>
 
 <section class="projects">
-    <div class="project-list" />
+    <h2 class="sideHeading">Projects</h2>
+    <div class="project-list">
+        <div class="project">
+            <p>Web development & Design</p>
+            <a href="#">Project #1</a>
+            <img src={Barcode} alt="">
+            <span class="line"></span>
+        </div>
+        <div class="project">
+            <p>Web development & Design</p>
+            <a href="#">Project #1</a>
+            <img src={Barcode} alt="">
+            <span class="line"></span>
+        </div>
+        <div class="project">
+            <p>Web development & Design</p>
+            <a href="#">Project #1</a>
+            <img src={Barcode} alt="">
+            <span class="line"></span>
+        </div>
+        <div class="project">
+            <p>Web development & Design</p>
+            <a href="#">Project #1</a>
+            <img src={Barcode} alt="">
+            <span class="line"></span>
+        </div>
+        <div class="project">
+            <p>Web development & Design</p>
+            <a href="#">Project #1</a>
+            <img src={Barcode} alt="">
+            <span class="line"></span>
+        </div>
+    </div>
+    <div class="preview">
+        <img src={Logo} alt="">
+    </div>
 </section>
 
 <style lang="scss">
@@ -46,7 +82,7 @@
 
     .barcode {
         position: absolute;
-        bottom: -56px;
+        bottom: 0;
         left: 50%;
         transform: translateX(-50%);
 
@@ -62,5 +98,179 @@
             text-transform: uppercase;
             font-family: "Krona";
         }
+
+        img {
+            height: 213px;
+        }
     }
+
+    .projects {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+
+        .project-list {
+            height: 600px;
+            overflow-y: scroll;
+            aspect-ratio: 1 / 1;
+            display: flex;
+            flex-direction: column-reverse;
+            justify-content: start;
+
+            .project {
+                color: white;
+                font-family: "Krona";
+                display: flex;
+                flex-direction: column;
+                row-gap: 8px;
+                position: relative;
+                padding: 20px 20px 24px 20px;
+                background: black;
+                transition: margin 0.5s ease-in-out;
+                
+                &:not(:last-of-type) {
+                    margin-top: -64px;
+                }
+
+                p{
+                    text-transform: uppercase;
+                    font-size: 16px;
+                }
+
+                a {
+                    font-size: 24px;
+                    color: white;
+                    text-decoration: none;
+                }
+
+                img {
+                    width: 100%;
+                    height: 48px;
+                    margin-top: 8px;
+                }
+
+                span.line {
+                    position: absolute;
+                    bottom: -10px;
+                    left: 0;
+                    height: 32px;
+                    width: 1.5px;
+                    transform: rotate(-45deg);
+                    transform-origin: top left;
+                    background: white;
+                }
+
+                &:hover {
+                    &:after {
+                        background: #F4D314;
+
+                    }
+
+                    &:hover:before {
+                        background: #F4D314;
+
+                    }
+
+                    .line {
+                        background: #F4D314;
+
+                    }
+
+                    a {
+                        color: #F4D314;
+                    }
+
+                    margin-top: 0;
+                }
+
+                &:after {
+                    content:"";
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    bottom: 22px;
+                    width: 1px;
+                    background: white;
+                }
+
+                &:before {
+                    content:"";
+                    position: absolute;
+                    bottom: 0;
+                    left: 22px;
+                    right: 0;
+                    height: 1px;
+                    background: white;
+                }
+            }
+        }
+
+        .preview {
+            width: 600px;
+            aspect-ratio: 1 / 1;
+            border: 1px solid white;
+            position: relative;
+
+            &:after {
+                content:"Awaiting user input...";
+                position: absolute;
+                top: calc(50% - 200px);
+                left: 50%;
+                transform: translate(-50%, -50%);
+                color: white;
+                opacity: 0.5;
+                font-family: "Krona";
+                text-transform: uppercase;
+                font-size: 18px;
+                width: 100%;
+                text-align: center;
+            }
+
+            &:before {
+                content:"Preview";
+                position: absolute;
+                top: -32px;
+                left: 0;
+                color: white;
+                opacity: 1;
+                font-family: "Kleenmax";
+                text-transform: uppercase;
+                font-size: 18px;
+                width: 100%;
+            }
+
+            img {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                opacity: 0.1;
+            }
+        }
+    }
+
+    section {
+        position: relative;
+        height: 100dvh;
+        padding: 0 326px;
+
+        .sideHeading {
+            position: absolute;
+            left: 110px;
+            top: 50%;
+            transform: translateY(-50%);
+            writing-mode: sideways-lr;
+            font-size: 64px;
+            color: transparent;
+            -webkit-text-stroke: 1px white;
+            text-transform: uppercase;
+            font-family: "Kleenmax";
+        }
+    }
+
+    section:not(:first-of-type) {
+            margin-top: 128px;
+    }
+
 </style>
